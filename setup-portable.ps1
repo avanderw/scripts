f77ebe601aa35c784c6b18d0efa7fc7712f1b8f9
Andrew van der Westhuizen <avanderw@gmail.com>
2018-02-13 13:43:48 +0200

    added openttd, still need to script getting config file and gfx

---
+++ setup-portable.ps1
@@ -0,0 +1,16 @@
+$OPEN_TTD="https://binaries.openttd.org/releases/1.7.2/openttd-1.7.2-windows-win64.zip"
+
+Write-Host "Configuring OpenTTD ..."
+
+if(!(Test-Path -Path "$PSScriptRoot\OpenTTD" )){
+    Write-Host "Downloading OpenTTD ..."
+    (New-Object System.Net.WebClient).DownloadFile("$OPEN_TTD", "$PSScriptRoot\OpenTTD.zip")
+
+    Write-Host "Extracting OpenTTD ..."
+    Add-Type -AssemblyName System.IO.Compression.FileSystem
+    [System.IO.Compression.ZipFile]::ExtractToDirectory("$PSScriptRoot\OpenTTD.zip", "$PSScriptRoot\OpenTTD")
+
+    Remove-Item -path "$PSScriptRoot\OpenTTD.zip"
+}
+
+Write-Host "... Configured OpenTTD"
