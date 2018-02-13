688d520c0a943a81cf499e9620e7d12f15340723
Andrew van der Westhuizen <avanderw@gmail.com>
2018-02-13 12:00:01 +0200

    updated readme with bat file execution

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # scripts
+
+	@powershell -NoProfile -ExecutionPolicy Bypass -Command "[System.Net.WebRequest]::DefaultWebProxy.Credentials = [System.Net.CredentialCache]::DefaultCredentials; iex ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/avanderw/scripts/master/setup-development.ps1'))"
--- setup-development.ps1
+++ setup-development.ps1
@@ -1,7 +1,8 @@
 $CHOCO_PATH="%ALLUSERSPROFILE%\chocolatey\bin\"
+
 Write-Host "Configuring chocolatey..."
 if(!(Test-Path -Path $CHOCO_PATH )){
-	Write-Debug "Installing chocolatey..."
+	Write-Host "Installing chocolatey..."
 	#[System.Net.WebRequest]::DefaultWebProxy.Credentials = [System.Net.CredentialCache]::DefaultCredentials; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 }
-Write-Debug "... Chocolatey configured"
+Write-Host "... Chocolatey configured"
